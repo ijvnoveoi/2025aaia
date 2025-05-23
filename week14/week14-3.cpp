bce9d0ad6ad10f9ff55c30296c52b7ad3a2ff884
ijvnoveoi <diegohan1008@gmail.com>
2025-05-23 12:04:18 +0800

    week14

---
+++ week14/week14-3.cpp
@@ -0,0 +1,19 @@
+///week14-3.cpp
+///UVA11063: B2-seqence
+///Part1:input, Part2:output
+#include <stdio.h>
+int main()
+{
+	int a[100];
+	int N; t = 1;
+	while( scanf("%d", &N) == 1 ){//Part1:Part1
+		for(int i=0; i<N; i++){//Part1:Part1
+			scanf("%d", &a[i] );//Part1:Part1
+		}
+
+		//Part2:Output
+		printf("Case #%d ; It is a B2-Sequence.\n\n", t);
+		t++;
+	}
+}
+
